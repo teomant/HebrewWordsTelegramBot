c5a7a1c2b1afd2cf98c253403a6af28b06b1e396
sobolev <sobolev@tecforce.ru>
2023-03-04 12:56:49 +0400

    Initial commit

---
+++ src/main/kotlin/crow/teomant/hebrewwordstelegrambot/HebreWordsTelegramBotApplication.kt
@@ -0,0 +1,11 @@
+package crow.teomant.hebrewwordstelegrambot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HebreWordsTelegramBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<HebreWordsTelegramBotApplication>(*args)
+}
---
+++ src/test/kotlin/crow/teomant/hebrewwordstelegrambot/HebrewWordsTelegramBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package crow.teomant.hebrewwordstelegrambot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HebrewWordsTelegramBotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
